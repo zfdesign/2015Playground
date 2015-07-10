3be56d25ffaf840c61f872b4a6567a0e3c643a1b
zlatin.ivanov <zlatin.ivanov@just-eat.com>
2015-07-10 13:49:13 +0100

    Setting master project to blanc

--- app/js/init.js
+++ app/js/init.js
@@ -1,9 +1,3 @@
-app.init  = function (data) {
-    new app.controllers.FeedController(data);
+app.init  = function () {
+    new app.controllers.{{NAME}}Controller();
 };
-(function () {
-    var tags = 'london';
-    var script = document.createElement('script');
-    script.src = 'http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=app.init&tags=' + tags;
-    document.head.appendChild(script);
-})();
